fb2ec09ea257dd7657808d53b21a0afb7af6de25
Rob <cribbie@yorku.ca>
2017-06-22 16:21:49 -0400

    modified eq.tost, changed equivint to ei, etc.

--- R/eq.tost.R
+++ R/eq.tost.R
@@ -5,18 +5,19 @@
 #' or the Schuirmann-Yuen test (if normality cannot be assumed), depending on the arguments specified. The original Schuirmann assumes 
 #' equal variances and normality. The Schuirmann-Welch assumes normality but not equal variances. The Schuirmann-Yuen accounts for 
 #' unequal variances and nonnormality using trimmed means and Winsorized variances.
-#' @aliases tost
+#' @aliases eq.tost
 #' 
-#' x, y, equivint, varequiv = FALSE, normality = FALSE, tr = 0.2, alpha = 0.05,  na.rm = TRUE, ...
+#' x, y, ei, varequiv = FALSE, normality = FALSE, tr = 0.2, alpha = 0.05,  na.rm = TRUE, print = TRUE, ...
 #' 
 #' @param x a numeric vector for first sample
 #' @param y a numeric vector for the second sample
-#' @param equivint numeric value defining the size of the equivalance interval
+#' @param ei numeric value defining the size (half-width) of the symmetric (around 0) equivalance interval
 #' @param varequal logical; If true, equal variances are assumed. Only applicable when tr == 0
 #' @param normality logical; If true, normality of x and y are assumed. 
-#' @param tr proportion of data to trim. When \code{tr == 0}, equivintther the standard Schuirmann test
+#' @param tr proportion of data to trim from each tail of each distribution (i.e., symmetric trimming). When \code{tr == 0}, the standard Schuirmann test
 #'  or Schuirmann-Welch is performed
-#' @param alpha the appropriate alpha level
+#' @param alpha the maximum allowable Type I error rate
+#' @param print whether or not to print a graphic of the results
 #' @param ... additional arguments to be passed
 #' 
 #' @return returns a \code{list} 
@@ -36,19 +37,22 @@
 #' y <- rnorm(100, 1)
 
 #' # Original Schuirman TOST
-#' eq.tost(x, y, equivint=0.5, alpha = 0.05, varequiv=FALSE, normality=FALSE)
+#' eq.tost(x, y, ei=0.5, alpha = 0.05, varequiv=FALSE, normality=FALSE)
 
 #' # Schuirmann-Welch
-#' eq.tost(x, y, equivint=0.5, alpha = 0.05, varequiv=FALSE, normality=TRUE)
+#' eq.tost(x, y, ei=0.5, alpha = 0.05, varequiv=FALSE, normality=TRUE)
 
 #' # Schuirmann-Yuen TOST (default)
-#' eq.tost(x, y, equivint=0.5, alpha = 0.05)
+#' eq.tost(x, y, ei=0.5, alpha = 0.05)
+
+#' #' # Schuirmann-Yuen TOST (10% symmetric trimming)
+#' eq.tost(x, y, ei=0.5, varequiv=FALSE, normality=FALSE, alpha = 0.05) 
 
 #' }
 
-eq.tost <- function(x, y, equivint, varequiv = FALSE, 
+eq.tost <- function(x, y, ei, varequiv = FALSE, 
     normality = FALSE, tr = 0.2, alpha = 0.05, na.rm = TRUE, 
-    ...) {
+    print = TRUE, ...) {
     if (na.rm) {
         x <- x[!is.na(x)]
         y <- y[!is.na(y)]
@@ -56,8 +60,8 @@ eq.tost <- function(x, y, equivint, varequiv = FALSE,
     if (normality) {
         if (varequiv == FALSE) {
             denom <- sqrt((var(x)/length(x)) + (var(y)/length(y)))
-            t1 <- (mean(x) - mean(y) - equivint)/denom
-            t2 <- (mean(x) - mean(y) + equivint)/denom
+            t1 <- (mean(x) - mean(y) - ei)/denom
+            t2 <- (mean(x) - mean(y) + ei)/denom
             dft <- (((var(x)/length(x)) + (var(y)/length(y)))^2)/((var(x)^2/(length(x)^2 * 
                 (length(x) - 1))) + (var(y)^2/(length(y)^2 * 
                 (length(y) - 1))))
@@ -74,8 +78,8 @@ eq.tost <- function(x, y, equivint, varequiv = FALSE,
                 ((length(y) - 1) * sd(y)^2))/(length(x) + 
                 length(y) - 2)) * (1/length(x) + 
                 1/length(y)))
-            t1 <- (mean(x) - mean(y) - equivint)/denom
-            t2 <- (mean(x) - mean(y) + equivint)/denom
+            t1 <- (mean(x) - mean(y) - ei)/denom
+            t2 <- (mean(x) - mean(y) + ei)/denom
             dft <- length(x) + length(y) - 2
             probt1 <- pt(t1, dft, lower.tail = T)
             probt2 <- pt(t2, dft, lower.tail = F)
@@ -98,8 +102,8 @@ eq.tost <- function(x, y, equivint, varequiv = FALSE,
         dft <- (q1 + q2)^2/((q1^2/(h1 - 1)) + (q2^2/(h2 - 
             1)))
         crit <- qt(1 - alpha/2, dft)
-        dif1 <- mean(x, tr) - mean(y, tr) - equivint
-        dif2 <- mean(x, tr) - mean(y, tr) + equivint
+        dif1 <- mean(x, tr) - mean(y, tr) - ei
+        dif2 <- mean(x, tr) - mean(y, tr) + ei
         t1 <- dif1/sqrt(q1 + q2)
         t2 <- dif2/sqrt(q1 + q2)
         probt1 <- pt(t1, dft)
@@ -117,8 +121,8 @@ eq.tost <- function(x, y, equivint, varequiv = FALSE,
         "Trimmed Mean Grp 2")
     sds <- c(sd(x), sd(y))
     names(sds) <- c("SD Grp 1", "SD Grp 2")
-    equivint <- (c(equivint))
-    names(equivint) <- c("equivalence interval")
+    ei <- (c(ei))
+    names(ei) <- c("equivalence interval")
     tstats <- c(t1, t2)
     names(tstats) <- c("t1", "t2")
     dfs <- c(dft, dft)
@@ -126,7 +130,7 @@ eq.tost <- function(x, y, equivint, varequiv = FALSE,
     pvals <- c(probt1, probt2)
     names(pvals) <- c("p_t1", "p_t2")
     res <- list(title, means = means, trimmeans = trimmeans, 
-        sds = sds, equivint = equivint, tstats = tstats, 
+        sds = sds, ei = ei, tstats = tstats, 
         dfs = dfs, pvals = pvals, decis = decis)
     return(res)
 }
@@ -139,7 +143,7 @@ print.eq.tost <- function(x, ...) {
     cat("Means:", x$means, "\n")
     cat("SDs:", x$sds, "\n")
     cat("Trimmed Means:", x$trimmeans, "\n")
-    cat("The equivalence interval was ", x$equivint, 
+    cat("The equivalence interval was ", x$ei, 
         "in unstandardized metric.")
     cat("Test statistics: ", x$tstats, "\n")
     cat("Degrees of freedom: ", x$dfs, "\n")
